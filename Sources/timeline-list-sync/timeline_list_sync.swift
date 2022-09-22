e3ec9faf5a2869319b55ce84536671ae264d22c4
PetitStrawberry <deloreantaro@yahoo.co.jp>
2022-09-22 22:50:34 +0900

    setup: swift package dependencies

---
+++ Sources/timeline-list-sync/timeline_list_sync.swift
@@ -0,0 +1,10 @@
+import TwitterAPIKit
+
+@main
+public struct TimelineListSync {
+    public private(set) var text = "Hello, World!"
+
+    public static func main() {
+        print(TimelineListSync().text)
+    }
+}
---
+++ Tests/timeline-list-syncTests/timeline_list_syncTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import timeline_list_sync
+
+final class TimelineListSyncTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(TimelineListSync().text, "Hello, World!")
+    }
+}
