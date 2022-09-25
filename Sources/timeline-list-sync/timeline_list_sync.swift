276736a4b487f9ff44c8a8bc06426fd5dfe44dc4
petitstrawberry <petitstrawberry.dev@gmail.com>
2022-09-25 18:34:22 +0900

    add: env vars (AccessToken+Secret)

--- Sources/timeline-list-sync/timeline_list_sync.swift
+++ Sources/timeline-list-sync/timeline_list_sync.swift
@@ -13,16 +13,18 @@ public struct TimelineListSync {
     public private(set) var text = "Hello, World!"
     private static let apiKey = ProcessInfo.processInfo.environment["TWITTER_API_KEY"]
     private static let apiSecret = ProcessInfo.processInfo.environment["TWITTER_API_SECRET"]
-    private static let bearerToken = ProcessInfo.processInfo.environment["TWITTER_BEARER_TOKEN"]
-
+    private static let accessToken = ProcessInfo.processInfo.environment["TWITTER_ACCESS_TOKEN"]
+    private static let accessTokenSecret = ProcessInfo.processInfo.environment["TWITTER_ACCESS_TOKEN_SECRET"]
+    
+    
     public static func main() {
         print(TimelineListSync().text)
 
         let client = TwitterAPIClient(.oauth10a(.init(
             consumerKey: apiKey!,
             consumerSecret: apiSecret!,
-            oauthToken: "",
-            oauthTokenSecret: ""
+            oauthToken: accessToken!,
+            oauthTokenSecret: accessTokenSecret!
         )))
     }
 }
